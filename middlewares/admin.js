9007e2f3d69c8c4fe9431cf7a9cbef3f1ac6d21d
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-02-13 23:58:55 +0530

    db-added

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -18,5 +18,25 @@ function adminMiddleware(req, res, next) {
     res.json({ msg: "invalid login inputs" });
   }
 }
+const { Admin } = require("../db/db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+function adminMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+  const word = token.split(" ");
+  const jwtToken = word[1];
+  try {
+    const decocdedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if (decocdedValue.username) {
+      req.username = decocdedValue.username;
+      next();
+    } else {
+      res.status(403).json({ msg: "Admin is not authenticated" });
+    }
+  } catch (error) {
+    res.json({ msg: "invalid login inputs" });
+  }
+}
 
 module.exports = adminMiddleware;
